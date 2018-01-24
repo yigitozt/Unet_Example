679c908f7c4869b33d734805702c3bc4ee94a607
yigit ozturk <ygtozt@gmail.com>
2018-01-24 03:16:16 +0300

    Initial Commit
    First Commit

---
+++ Assets/ZZScript/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void OnCollisionEnter(Collision c)
+	{
+		Health health = c.gameObject.GetComponent<Health> ();
+
+		if (health != null) {
+			health.TakeDamage(10);
+		}
+
+		Destroy (gameObject);
+	} 
+}
---
+++ Assets/ZZScript/Health.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class Health : NetworkBehaviour {
+
+	public const int maxHealth = 100;
+
+	[SyncVar(hook = "OnChangeHealth")]
+	public int currentHealth = maxHealth;
+	public RectTransform healthBar;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public void TakeDamage(int amount)
+	{
+		if (!isServer)
+		{
+			return;
+		}
+
+		currentHealth -= amount;
+		if (currentHealth <= 0)
+		{
+			currentHealth = maxHealth;
+
+			// called on the Server, but invoked on the Clients
+			RpcRespawn();
+		}
+	}
+
+	void OnChangeHealth (int health)
+	{
+		healthBar.sizeDelta = new Vector2(health, healthBar.sizeDelta.y);
+	}
+
+	[ClientRpc]
+	void RpcRespawn()
+	{
+		if (isLocalPlayer)
+		{
+			// move back to zero location
+			transform.position = Vector3.zero;
+		}
+	}
+}
